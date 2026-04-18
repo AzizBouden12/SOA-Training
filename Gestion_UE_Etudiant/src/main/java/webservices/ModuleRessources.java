2eba076ac6b8d916f9099b1f87195a3e5f93a8b5
ABouden <abouden@mindstate.tech>
2026-04-18 16:37:41 +0100

    workshop

---
+++ Gestion_UE_Etudiant/src/main/java/webservices/ModuleRessources.java
@@ -0,0 +1,106 @@
+﻿package webservices;
+
+import javax.ws.rs.GET; 
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path; 
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType; 
+import javax.ws.rs.core.Response;
+import metiers.ModuleBusiness;
+import entities.Module;
+import entities.UniteEnseignement;
+
+@Path("/module")
+public class ModuleRessources {
+    public ModuleBusiness moduleBusiness = new ModuleBusiness();
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getListeModules() {
+        return Response
+                .status(200)
+                .entity(moduleBusiness.getAllModules())
+                .build();
+    }
+
+    @GET
+    @Path("/{type}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getModuleByType(@PathParam("type") Module.TypeModule type) {
+        return Response
+                .status(200)
+                .entity(moduleBusiness.getModulesByType(type))
+                .build();
+    }
+
+    @GET
+    @Path("/{domaine}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getModuleByMatricule(@PathParam("domaine") String matricule) {
+        return Response
+                .status(200)
+                .entity(moduleBusiness.getModuleByMatricule(matricule))
+                .build();
+    }
+
+    @GET
+    @Path("/{ue}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getModuleByUE(@PathParam("ue") UniteEnseignement ue) {
+        return Response
+                .status(200)
+                .entity(moduleBusiness.getModulesByUE(ue))
+                .build();
+    }
+
+    @POST
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addModule(Module module) {
+        boolean success = moduleBusiness.addModule(module);
+        if (success) {
+            return Response.status(201)
+                    .entity("Module ajoutÃ©")
+                    .build();
+        } else {
+            return Response.status(500)
+                    .entity("Ajout Module fail").build();
+        }
+    }
+
+    @PUT
+    @Path("/update/{code}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateModule(@PathParam("code") int code, Module updatedModule) {
+        boolean success = moduleBusiness.updateModule(updatedModule.getMatricule(), updatedModule);
+        if (success) {
+            return Response.status(200)
+                    .entity("Module mis Ã  jour")
+                    .build();
+        } else {
+            return Response.status(404)
+                    .entity("Module non trouvÃ©").build();
+        }
+    }
+
+    @DELETE
+    @Path("/delete/{matricule}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteModule(@PathParam("matricule") String matricule) {
+        boolean success = moduleBusiness.deleteModule(matricule);
+        if (success) {
+            return Response.status(200)
+                    .entity("Module supprimÃ©")
+                    .build();
+        } else {
+            return Response.status(404)
+                    .entity("Module non trouvÃ©").build();
+        }
+    }
+
+
+}
---
+++ Gestion_UE_Etudiant/src/main/java/webservices/UERessources.java
@@ -0,0 +1,105 @@
+﻿package webservices;
+
+import javax.ws.rs.GET; 
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.Path; 
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType; 
+import javax.ws.rs.core.Response;
+import metiers.UniteEnseignementBusiness;
+import entities.UniteEnseignement;
+
+@Path("/UE")
+public class UERessources {
+    public UniteEnseignementBusiness ueBusiness = new UniteEnseignementBusiness();
+
+    @GET
+    @Path("/getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getListeUE() {
+        return Response
+                .status(200)
+                .entity(ueBusiness.getListeUE())
+                .build();
+    }
+
+    @GET
+    @Path("/{semestre}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUEBySemestre(@PathParam("semestre") int semestre) {
+        return Response
+                .status(200)
+                .entity(ueBusiness.getUEBySemestre(semestre))
+                .build();
+    }
+
+    @GET
+    @Path("/{domaine}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUEByDomaine(@PathParam("domaine") String domaine) {
+        return Response
+                .status(200)
+                .entity(ueBusiness.getUEByDomaine(domaine))
+                .build();
+    }
+
+    @GET
+    @Path("/{code}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUEByCode(@PathParam("code") int code) {
+        return Response
+                .status(200)
+                .entity(ueBusiness.getUEByCode(code))
+                .build();
+    }
+
+    @POST
+    @Path("/add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addUE(UniteEnseignement ue) {
+        boolean success = ueBusiness.addUniteEnseignement(ue);
+        if (success) {
+            return Response.status(201)
+                    .entity("UE ajoutÃ©e")
+                    .build();
+        } else {
+            return Response.status(500)
+                    .entity("Ajout UE fail").build();
+        }
+    }
+
+    @PUT
+    @Path("/update/{code}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUE(@PathParam("code") int code, UniteEnseignement updatedUE) {
+        boolean success = ueBusiness.updateUniteEnseignement(code, updatedUE);
+        if (success) {
+            return Response.status(200)
+                    .entity("UE mise Ã  jour")
+                    .build();
+        } else {
+            return Response.status(404)
+                    .entity("UE non trouvÃ©e").build();
+        }
+    }
+
+    @DELETE
+    @Path("/delete/{code}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteUE(@PathParam("code") int code) {
+        boolean success = ueBusiness.deleteUniteEnseignement(code);
+        if (success) {
+            return Response.status(200)
+                    .entity("UE supprimÃ©e")
+                    .build();
+        } else {
+            return Response.status(404)
+                    .entity("UE non trouvÃ©e").build();
+        }
+    }
+
+
+}
